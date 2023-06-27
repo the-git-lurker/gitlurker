b3fc6fbddf3379757744c74b3bdc968e4d96141a
Mynima <i.p.sanderson@gmail.com>
2023-06-27 22:25:58 +0100

    SU update

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsu
-python manage.py initial_db_content
+python manage.py createsuperuser --noinput
