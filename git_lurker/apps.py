a0e40bc223d9bf86d340ee3420d8ec41aa4ce5a5
Mynima <i.p.sanderson@gmail.com>
2023-06-28 07:12:12 +0100

    updated settings

--- git_lurker/apps.py
+++
@@ -1,5 +0,0 @@
-class GitLurkerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'git_lurker'
-    verbose_name = 'Git Lurker'
-    fixtures = ['initial_projects.json']
