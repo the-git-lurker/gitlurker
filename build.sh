1e9f463a265a2fb73bc824144bdf5405c8e0c240
Mynima <i.p.sanderson@gmail.com>
2023-06-28 07:02:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createsuperuser --noinput
+python manage.py migrate git_lurker zero
 python manage.py migrate
 python manage.py dumpdata git_lurker.project > initial_projects.json
+python manage.py createsuperuser --noinput
