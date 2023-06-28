81fb06b732e6c434ae044d6758f47c30831ea614
Mynima <i.p.sanderson@gmail.com>
2023-06-28 06:54:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput
+python manage.py migrate
 python manage.py dumpdata git_lurker.project > initial_projects.json
