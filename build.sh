9ab50c98fc2a50c63d40cf0d69f98c543f99f70c
Mynima <i.p.sanderson@gmail.com>
2023-06-28 06:22:31 +0100

    Add fixture
    Removed management folder as SU is done via env variables
    Add fixture

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput
+python manage.py dumpdata
---
+++ git_lurker/fixtures/initital_projects.py
@@ -0,0 +1,202 @@
+[
+  {
+    "model": "git_lurker.project",
+    "pk": 1,
+    "fields": {
+      "repository": "bitcoin",
+      "owner": "bitcoin"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 2,
+    "fields": {
+      "repository": "lndconnect",
+      "owner": "LN-Zap"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 3,
+    "fields": {
+      "repository": "lnbits",
+      "owner": "lnbits"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 4,
+    "fields": {
+      "repository": "cashu",
+      "owner": "lnbits"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 5,
+    "fields": {
+      "repository": "mempool",
+      "owner": "mempool"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 6,
+    "fields": {
+      "repository": "sparrow",
+      "owner": "sparrowwallet"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 7,
+    "fields": {
+      "repository": "robosats",
+      "owner": "Robosats"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 8,
+    "fields": {
+      "repository": "joinmarket-clientserver",
+      "owner": "JoinMarket-Org"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 9,
+    "fields": {
+      "repository": "jam",
+      "owner": "joinmarket-webui"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 10,
+    "fields": {
+      "repository": "specter-desktop",
+      "owner": "cryptoadvance"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 11,
+    "fields": {
+      "repository": "start-os",
+      "owner": "Start9Labs"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 12,
+    "fields": {
+      "repository": "RTL",
+      "owner": "Ride-The-Lightning"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 13,
+    "fields": {
+      "repository": "lnd",
+      "owner": "lightningnetwork"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 14,
+    "fields": {
+      "repository": "lightning",
+      "owner": "ElementsProject"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 15,
+    "fields": {
+      "repository": "electrs",
+      "owner": "romanz"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 16,
+    "fields": {
+      "repository": "btc-rpc-explorer",
+      "owner": "janoside"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 17,
+    "fields": {
+      "repository": "btcpayserver",
+      "owner": "btcpayserver"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 18,
+    "fields": {
+      "repository": "bluewallet",
+      "owner": "bluewallet"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 19,
+    "fields": {
+      "repository": "raspibolt",
+      "owner": "raspibolt"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 20,
+    "fields": {
+      "repository": "raspiblitz",
+      "owner": "raspiblitz"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 21,
+    "fields": {
+      "repository": "umbrel",
+      "owner": "getumbrel"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 22,
+    "fields": {
+      "repository": "zeus",
+      "owner": "ZeusLN"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 23,
+    "fields": {
+      "repository": "eclair",
+      "owner": "ACINQ"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 24,
+    "fields": {
+      "repository": "phoenix",
+      "owner": "ACINQ"
+    }
+  },
+  {
+    "model": "git_lurker.project",
+    "pk": 25,
+    "fields": {
+      "repository": "nunchuk-desktop",
+      "owner": "nunchuk-io"
+    }
+  }
+]
