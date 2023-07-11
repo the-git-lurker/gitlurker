44a592e57b4f5e1f9ff23cffc8f92c9653888e72
Mynima <i.p.sanderson@gmail.com>
2023-07-11 21:16:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-python manage.py reset git_lurker
-python manage.py syncdb
+reset_db --noinput
 python manage.py collectstatic --no-input
 python manage.py makemigrations git_lurker --empty
 python manage.py makemigrations git_lurker
