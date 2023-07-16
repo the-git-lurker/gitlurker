01ed97f637d02a671f6ef6c0a2281ba1f3ba1157
Mynima <62206239+Mynima@users.noreply.github.com>
2023-07-16 16:40:06 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate
-python manage.py loaddata initial_projects.json
-# python manage.py createsuperuser --noinput
+# python manage.py loaddata initial_projects.json
+python manage.py createsuperuser --noinput
