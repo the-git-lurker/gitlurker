fb4873a2735df0c6f3dee97c3555a60791ba261d
Mynima <62206239+Mynima@users.noreply.github.com>
2024-04-01 16:58:18 +0100

    update build script

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py reset_db --noinput
 python manage.py makemigrations git_lurker --no-input
-python manage.py migrate git_lurker
+python manage.py migrate git_lurker --run-syncdb 
 python manage.py loaddata initial_projects_categorised.json
-# python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput
