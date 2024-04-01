c9788245563a0392571f2f522034c9c6269920a8
Mynima <62206239+Mynima@users.noreply.github.com>
2024-04-01 19:12:00 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations git_lurker --no-input
-python manage.py migrate git_lurker --run-syncdb 
+python manage.py migrate git_lurker
 python manage.py loaddata initial_projects_categorised.json
-python manage.py createsuperuser --noinput
+# python manage.py createsuperuser --noinput
